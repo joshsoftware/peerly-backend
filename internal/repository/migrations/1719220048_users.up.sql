1ddbf369807bbf7c0336fb1bb82b16d49695dcc3
Samnit Patil <samnitpatil9882@gmail.com>
2024-06-25 15:10:44 +0530

    [feature/PEER-16-DatabaseMigrations]added  organization_config table and deleted organization table

--- internal/repository/migrations/1719220048_users.up.sql
+++ internal/repository/migrations/1719220048_users.up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
-    org_id BIGINT NOT NULL REFERENCES organizations(id),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL, 
+    password VARCHAR(255) , 
     profile_image_url TEXT,
     designation VARCHAR(255) NOT NULL, 
     reward_quota_balance INT NOT NULL,
@@ -12,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users (
     role_id INT NOT NULL REFERENCES roles(id),
     grade_id INT NOT NULL REFERENCES grades(id),
     created_at BIGINT DEFAULT (EXTRACT(EPOCH FROM NOW()) * 1000)::BIGINT,
-    created_by BIGINT NOT NULL REFERENCES users(id),
+    created_by BIGINT REFERENCES users(id),
     updated_at BIGINT,
-    updated_by BIGINT
+    updated_by BIGINT REFERENCES users(id)
 );
--- internal/repository/migrations/1719220922_appreciations.up.sql
+++ internal/repository/migrations/1719220922_appreciations.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS appreciations (
     id SERIAL PRIMARY KEY,
     core_value_id INT NOT NULL REFERENCES core_values(id),
     description TEXT NOT NULL, -- reason
-    is_valid BOOLEAN NOT NULL,
+    is_valid BOOLEAN NOT NULL DEFAULT true,
     total_rewards INT,
     quarter INT NOT NULL,
     sender BIGINT NOT NULL REFERENCES users(id),
