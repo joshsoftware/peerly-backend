540f96646816c37ed64142cd8ebaedf6677806cb
Samnit Patil <samnitpatil9882@gmail.com>
2024-08-08 19:13:12 +0530

    [feature/addEmailAndNotification] added notification tokens table in migration file

---
+++ internal/repository/migrations/1723124158_notification_tokens.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS notification_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    notification_token TEXT NOT NULL,
+    UNIQUE (user_id, notification_token)
+);
