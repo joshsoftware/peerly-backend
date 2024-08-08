3f19703465d4ca83c5c90650c86225278d01f7e8
Samnit Patil <samnitpatil9882@gmail.com>
2024-08-09 03:35:17 +0530

    [Deployment] testing notifications

--- internal/app/cronjob/monthlyUpdateData.go
+++ internal/app/cronjob/monthlyUpdateData.go
@@ -14,8 +14,8 @@ const MONTHLY_JOB = "MONTHLY_JOB"
 const MONTHLY_CRON_JOB_INTERVAL_MONTHS = 1
 
 var MonthlyJobTiming = JobTime{
-	hours:   0,
-	minutes: 0,
+	hours:   3,
+	minutes: 35,
 	seconds: 0,
 }
 
@@ -37,7 +37,7 @@ func NewMontlyJob(userSvc user.Service,scheduler gocron.Scheduler) Job {
 func (cron *MonthlyJob) Schedule() {
 	var err error
 	cron.job, err = cron.scheduler.NewJob(
-		gocron.MonthlyJob(MONTHLY_CRON_JOB_INTERVAL_MONTHS, gocron.NewDaysOfTheMonth(-1),
+		gocron.MonthlyJob(MONTHLY_CRON_JOB_INTERVAL_MONTHS, gocron.NewDaysOfTheMonth(9),
 			gocron.NewAtTimes(
 				gocron.NewAtTime(
 					MonthlyJobTiming.hours,
