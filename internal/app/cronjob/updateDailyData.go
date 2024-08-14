0969014c8b8c57b3865b4387b80c38c6204075fa
Samnit Patil <samnitpatil9882@gmail.com>
2024-08-14 10:58:43 +0530

    [Deployment] updatedaily job condition reset

--- internal/app/cronjob/updateDailyData.go
+++ internal/app/cronjob/updateDailyData.go
@@ -13,8 +13,8 @@ const DAILY_JOB = "DAILY_JOB"
 const SAY_HELLO_DAILY_CRON_JOB_INTERVAL_DAYS = 1
 
 var SayHelloDailyJobTiming = JobTime{
-	hours:   0,
-	minutes: 0,
+	hours:   5,
+	minutes: 30,
 	seconds: 0,
 }
 
