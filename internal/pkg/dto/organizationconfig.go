1223e9d3ca8d03fae93f48657738523254fecfa2
Samnit Patil <samnitpatil9882@gmail.com>
2024-08-16 13:35:29 +0530

    [fixes/OrganizationTimezone] timezone changed to iana time zone

--- internal/pkg/dto/organizationconfig.go
+++ internal/pkg/dto/organizationconfig.go
@@ -1,34 +1,9 @@
 package dto
 import(
+	"time"
 	"github.com/joshsoftware/peerly-backend/internal/pkg/apperrors"
 )
-// map with all the time zones
-var timeZones = map[string]bool{
-	"NZDT":  true, "IDLE":  true, "NZST":  true, "NZT":   true,
-	"AESST": true, "ACSST": true, "CADT":  true, "SADT":  true,
-	"AEST":  true, "EAST":  true, "GST":   true, "LIGT":  true,
-	"SAST":  true, "CAST":  true, "AWSST": true, "JST":   true,
-	"KST":   true, "MHT":   true, "WDT":   true, "MT":    true,
-	"AWST":  true, "CCT":   true, "WADT":  true, "WST":   true,
-	"JT":    true, "ALMST": true, "WAST":  true, "CXT":   true,
-	"ALMT":  true, "MAWT":  true, "IOT":   true, "MVT":   true,
-	"TFT":   true, "AFT":   true, "MUT":   true,
-	"RET":   true, "SCT":   true, "IT":    true, "EAT":   true,
-	"BT":    true, "EETDST":true, "HMT":   true, "BDST":  true,
-	"CEST":  true, "CETDST":true, "EET":   true, "FWT":   true,
-	"IST":   true, "MEST":  true, "METDST":true, "SST":   true,
-	"BST":   true, "CET":   true, "DNT":   true, "FST":   true,
-	"MET":   true, "MEWT":  true, "MEZ":   true, "NOR":   true,
-	"SET":   true, "SWT":   true, "WETDST":true, "GMT":   true,
-	"UT":    true, "UTC":   true, "Z":     true, "ZULU":  true,
-	"WET":   true, "WAT":   true, "NDT":   true, "ADT":   true,
-	"AWT":   true, "NFT":   true, "NST":   true, "AST":   true,
-	"ACST":  true, "ACT":   true, "EDT":   true, "CDT":   true,
-	"EST":   true, "CST":   true, "MDT":   true, "MST":   true,
-	"PDT":   true, "AKDT":  true, "PST":   true, "YDT":   true,
-	"AKST":  true, "HDT":   true, "YST":   true, "AHST":  true,
-	"HST":   true, "CAT":   true, "NT":    true, "IDLW":  true,
-}
+
 
 type OrganizationConfig struct {
 	ID                          int64  `json:"id"`
@@ -51,7 +26,7 @@ func(orgConfig OrganizationConfig) OrgValidate() (err error)  {
 		return apperrors.InvalidRewardQuotaRenewalFrequency
 	}
 
-	if !isTimeZoneValid(orgConfig.Timezone) {
+	if !isValidTimeZone(orgConfig.Timezone) {
 		return apperrors.InvalidTimezone
 	}
 
@@ -61,7 +36,7 @@ func(orgConfig OrganizationConfig) OrgValidate() (err error)  {
 func (orgConfig OrganizationConfig) OrgUpdateValidate() (err error)  {
 
 	if orgConfig.Timezone != "" {
-		if !isTimeZoneValid(orgConfig.Timezone) {
+		if !isValidTimeZone(orgConfig.Timezone) {
 			return apperrors.InvalidTimezone
 		}
 	}
@@ -69,7 +44,8 @@ func (orgConfig OrganizationConfig) OrgUpdateValidate() (err error)  {
 	return
 }
 
-func isTimeZoneValid(tz string) bool {
-	_, exists := timeZones[tz]
-	return exists
+// Check if a given time zone is valid
+func isValidTimeZone(tz string) bool {
+	_, err := time.LoadLocation(tz)
+	return err == nil
 }
