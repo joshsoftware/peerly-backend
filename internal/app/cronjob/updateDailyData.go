b5db7d047aa861f43d47292e8a796a9ad624da93
Samnit Patil <samnitpatil9882@gmail.com>
2024-08-21 19:00:33 +0530

    [Deployment] reset monthly an daily update data timing

--- internal/app/cronjob/updateDailyData.go
+++ internal/app/cronjob/updateDailyData.go
@@ -12,8 +12,8 @@ const DAILY_JOB = "DAILY_JOB"
 const SAY_HELLO_DAILY_CRON_JOB_INTERVAL_DAYS = 1
 
 var SayHelloDailyJobTiming = JobTime{
-	hours:   18,
-	minutes: 55,
+	hours:   0,
+	minutes: 0,
 	seconds: 0,
 }
 
