61f9d99bf5d0a238d3f0ce7da9553dbe4b690429
irfanmohammad01 <irfan.mohammad@joshsoftware.com>
2026-07-03 10:06:07 +0530

    feat: implement cron.log file and refactored cron tasks to use it

--- internal/app/cronjob/monthlyUpdateData.go
+++ internal/app/cronjob/monthlyUpdateData.go
@@ -8,8 +8,6 @@ import (
 	"github.com/joshsoftware/peerly-backend/internal/app/notification"
 	orgSvc "github.com/joshsoftware/peerly-backend/internal/app/organizationConfig"
 	user "github.com/joshsoftware/peerly-backend/internal/app/users"
-	log "github.com/joshsoftware/peerly-backend/internal/pkg/logger"
-
 	logger "github.com/joshsoftware/peerly-backend/internal/pkg/logger"
 )
 
@@ -60,23 +58,23 @@ func (cron *MonthlyJob) Schedule() error {
 	)
 	cron.scheduler.Start()
 	if err != nil {
-		logger.Warn(context.TODO(), fmt.Sprintf("error occurred while scheduling %s, message %+v", cron.name, err.Error()))
+		logger.CronWarn(context.TODO(), fmt.Sprintf("error occurred while scheduling %s, message %+v", cron.name, err.Error()))
 		return err
 	}
 	return nil
 }
 
 func (cron *MonthlyJob) Task(ctx context.Context) {
-	logger.Info(ctx, "in monthly job task")
+	logger.CronInfo(ctx, "in monthly job task")
 	var err error
 	for i := 0; i < 3; i++ {
-		logger.Info(ctx, "cron job attempt:", i+1)
+		logger.CronInfo(ctx, "cron job attempt:", i+1)
 		err = cron.userService.UpdateRewardQuota(ctx)
 		if err == nil {
 			sendRewardQuotaRefilledNotificationToAll()
 			return
 		}
-		log.Info(ctx, fmt.Sprintf("cronjob fail error: %v", err.Error()))
+		logger.CronInfo(ctx, fmt.Sprintf("cronjob fail error: %v", err.Error()))
 	}
 	// return err
 	return
@@ -88,7 +86,7 @@ func sendRewardQuotaRefilledNotificationToAll() {
 		Body:  "Your reward quota is reset! You now recognize your colleagues.",
 	}
 
-	logger.Debug(context.Background(), "msg:", msg)
+	logger.CronDebug(context.Background(), "msg:", msg)
 	msg.SendNotificationToTopic("peerly")
 }
 
@@ -98,6 +96,6 @@ func (cron *MonthlyJob) setMonthlyInterval() error {
 		return err
 	}
 	MONTHLY_CRON_JOB_INTERVAL_MONTHS = orgInfo.RewardQuotaRenewalFrequency
-	log.Info(context.Background(), fmt.Sprintf("MONTHLY_CRON_JOB_INTERVAL_MONTHS = %d", MONTHLY_CRON_JOB_INTERVAL_MONTHS))
+	logger.CronInfo(context.Background(), fmt.Sprintf("MONTHLY_CRON_JOB_INTERVAL_MONTHS = %d", MONTHLY_CRON_JOB_INTERVAL_MONTHS))
 	return nil
 }
